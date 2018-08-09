6b97ee0b7a80c5b73caafa2488471ec4bf2082ea
Sam Wan <sam@gooddogdesign.com>
2018-08-10 01:06:28 +0930

    Added Tags for posts (Normally, I would just use Acts As Taggable On)

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,9 @@
 class Post < ApplicationRecord
   belongs_to :blog
   has_many  :reactions, dependent: :destroy
+  has_many  :post_tags
+  has_many  :tags, through: :post_tags
+
 
   validates :title, presence: true
   validates :content, presence: true
