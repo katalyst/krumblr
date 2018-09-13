fff3cfd1bcd3fb75f06cfc2cc23fa020ac96c07f
Sameer Subba <sameer.libang@gmail.com>
2018-09-13 19:03:48 +1000

    Post FactoryBot configuration

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -6,6 +6,7 @@ require File.expand_path('../../config/environment', __FILE__)
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
+require 'capybara/rails'
 require 'shoulda/matchers'
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
@@ -55,6 +56,9 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+
+  # include FactoryBot methods
+  config.include FactoryBot::Syntax::Methods
 end
 
 Shoulda::Matchers.configure do |config|
