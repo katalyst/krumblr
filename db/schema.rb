51bb9dc21477006bd28eb6bcc08ad11def540a30
Brett Laishley <brett.laishley@gmail.com>
2018-09-26 21:19:21 +0930

    add validation to database

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_24_110205) do
+ActiveRecord::Schema.define(version: 2018_09_26_080412) do
 
   create_table "blogs", force: :cascade do |t|
     t.text "text"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2018_09_24_110205) do
     t.integer "post_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["post_id", "user_id"], name: "index_likes_on_post_id_and_user_id", unique: true
     t.index ["post_id"], name: "index_likes_on_post_id"
     t.index ["user_id"], name: "index_likes_on_user_id"
   end
